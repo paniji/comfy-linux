a25e31ea301e3e321bfd29f00bd793da3506c62b
AWS CodeBuild <pani-aws-codebuild@ap.com>
2024-07-13 09:46:25 -0400

    v4

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ sudo systemctl restart flask-app
 sudo systemctl enable flask-app
 sudo systemctl status flask-app
 #curl -X POST http://127.0.0.1:8188/prompt -H "Content-Type: application/json" -d '{"message": "Hello, World!"}'
-#curl -X POST http://localhost:5000/prompt -H "Content-Type: application/json" -d '{"path":"https://cdn.pixabay.com/photo/2017/08/27/20/10/png-2687339_1280.png", "key":"d70297a8-e567-42bd-8ed6-7173b2daf30c"}'
+#curl -X POST http://localhost:8188/prompt -H "Content-Type: application/json" -d '{"path":"https://cdn.pixabay.com/photo/2017/08/27/20/10/png-2687339_1280.png", "key":"d70297a8-e567-42bd-8ed6-7173b2daf30c"}'
 
 
 
