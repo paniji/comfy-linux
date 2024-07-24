377fd73d1f2ef04d96565a614aca91087c26e478
AWS CodeBuild <pani-aws-codebuild@ap.com>
2024-07-23 20:58:41 -0400

    v7

--- comfy.sh
+++ comfy.sh
@@ -2,14 +2,23 @@
 # git clone https://github.com/paniji/comfy-linux.git
 # it pull https://github.com/paniji/comfy-linux.git 
 # cd comfy-linux
+
+source activate pytorch
+
+pip install einops
+pip install torchsde
+pip install aiohttp
+pip install kornia
+pip install spandrel
+
 python3 cpu/prep/comfy/comfy.py # Comfy Pre 
 bash cpu/prep/comfy/init.sh # Cloudflare etc
 
-sudo touch /var/log/comfyui_service.log
-sudo chown ec2-user:ec2-user /var/log/comfyui_service.log
+# sudo touch /var/log/comfyui_service.log
+# sudo chown ec2-user:ec2-user /var/log/comfyui_service.log
 
-# sudo cp cpu/svc/comfy/comfyui.service /etc/systemd/system/
-# sudo systemctl daemon-reload
-# sudo systemctl start comfyui
-# sudo systemctl enable comfyui
-# sudo systemctl status comfyui
+sudo cp cpu/svc/comfy/comfyui.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start comfyui
+sudo systemctl enable comfyui
+sudo systemctl status comfyui
