845243d151a425cc4aa4881e741a024a980d3150
AWS CodeBuild <pani-aws-codebuild@ap.com>
2024-07-24 20:20:09 -0400

    v10

--- comfy.sh
+++ comfy.sh
@@ -14,11 +14,18 @@ pip install spandrel
 python3 cpu/prep/comfy/comfy.py # Comfy Pre 
 bash cpu/prep/comfy/init.sh # Cloudflare etc
 
+wget -O AI/ComfyUI/models/checkpoints/wildcardxXLANIMATION.safetensors "https://civitai.com/api/download/models/357959?&token=4304d2c858702a4457d4d46c64e86420"
+wget -O AI/ComfyUI/models/loras/DreamyVibesArtsyle-SDXL-LoRA.safetensors "https://civitai.com/api/download/models/287607?&token=4304d2c858702a4457d4d46c64e86420"
+
 # sudo touch /var/log/comfyui_service.log
 # sudo chown ec2-user:ec2-user /var/log/comfyui_service.log
 
+## For ComfyUI service
 # sudo cp cpu/svc/comfy/comfyui.service /etc/systemd/system/
 # sudo systemctl daemon-reload
 # sudo systemctl start comfyui
 # sudo systemctl enable comfyui
 # sudo systemctl status comfyui
+
+## For UI access
+# python3 server.py
