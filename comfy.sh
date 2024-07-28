986fb92ba64120b2d37725930626b644722b5720
AWS CodeBuild <pani-aws-codebuild@ap.com>
2024-07-28 07:57:17 -0400

    v38

--- comfy.sh
+++ comfy.sh
@@ -19,6 +19,26 @@ bash cpu/prep/comfy/init.sh # Cloudflare etc
 wget -O AI/ComfyUI/models/checkpoints/wildcardxXLANIMATION.safetensors "https://civitai.com/api/download/models/357959?&token=4304d2c858702a4457d4d46c64e86420"
 wget -O AI/ComfyUI/models/loras/DreamyVibesArtsyle-SDXL-LoRA.safetensors "https://civitai.com/api/download/models/287607?&token=4304d2c858702a4457d4d46c64e86420"
 
+python3 --version
+python3 -m pip install --upgrade pip
+python3 -m venv myenv
+source myenv/bin/activate
+pip install boto3
+pip install requests
+deactivate
+
+sudo cp cpu/svc/comfy/s3-uploader.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable s3-uploader
+sudo systemctl start s3-uploader
+sudo systemctl status s3-uploader
+
+sudo cp cpu/svc/comfy/sqs-poller.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start sqs-poller
+sudo systemctl enable sqs-poller
+sudo systemctl status sqs-poller
+
 ## Upgrade python if needed for boto3 compatibility.. sometime python worl it out and fix lines below before the services
 # cd /usr/src
 # sudo wget https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz
@@ -34,12 +54,20 @@ wget -O AI/ComfyUI/models/loras/DreamyVibesArtsyle-SDXL-LoRA.safetensors "https:
 # pip install boto3
 # pip install requests
 
-# ComfyUI service
-sudo cp cpu/svc/comfy/comfyui.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl start comfyui
-sudo systemctl enable comfyui
-sudo systemctl status comfyui
+# python3 
+# python3 -m pip install --upgrade pip
+# python3 -m venv myenv
+# source myenv/bin/activate
+# pip install boto3
+# pip install requests
+# deactivate
+
+# # ComfyUI service
+# sudo cp cpu/svc/comfy/comfyui.service /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl start comfyui
+# sudo systemctl enable comfyui
+# sudo systemctl status comfyui
 
 # # S3 Uploader service
 # sudo cp cpu/svc/comfy/s3-uploader.service /etc/systemd/system/
